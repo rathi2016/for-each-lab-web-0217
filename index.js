e2424bbd6e3aef3b7d71a8745010e6d59d73ce77
github username <github email address>
2017-04-06 10:51:37 -0400

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function iterativeLog(array) {
+array.forEach((ele,idx)) => {
+    console.log(`${idx} ${ele}`)
+  }
+}
+
+function iterate(callback) {
+  const stuff = ['pen','dog','egg']
+  stuff.forEach(callback)
+  return stuff
+}
+
+function doToArray(arr,callback) {
+  arr.forEach(callback)
+}
